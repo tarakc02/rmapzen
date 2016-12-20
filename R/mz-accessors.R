9ff630815659523c1b19472df471d512ae079d54
tarakc02 <tarakc02@users.noreply.github.com>
2016-12-19 16:53:12 -0800

    ability to get bounding box from isochrone responses

--- R/mz-accessors.R
+++ R/mz-accessors.R
@@ -20,14 +20,17 @@ mz_coordinates <- function(geo) UseMethod("mz_coordinates")
 #' @export
 mz_bbox.mapzen_geo_list <- function(geo) {
     bbox <- geo$bbox
-    if (is.null(bbox) || length(bbox) != 4L) return(
-        tibble::data_frame(
-            min_lon = NA_real_,
-            min_lat = NA_real_,
-            max_lon = NA_real_,
-            max_lat = NA_real_
+    if (is.null(bbox) || length(bbox) != 4L) {
+        warning("Unable to read bounding box, returning NA")
+        return(
+            tibble::data_frame(
+                min_lon = NA_real_,
+                min_lat = NA_real_,
+                max_lon = NA_real_,
+                max_lat = NA_real_
+            )
         )
-    )
+    }
 
     tibble::data_frame(
         min_lon = as.numeric(bbox[[1]]),
@@ -35,6 +38,30 @@ mz_bbox.mapzen_geo_list <- function(geo) {
         max_lon = as.numeric(bbox[[3]]),
         max_lat = as.numeric(bbox[[4]])
     )
+}
+
+mz_bbox.mazpen_isochrone_list <- function(geo) {
+    features <- geo$features
+    default <- function() {
+        warning("Unable to read bounding box, returning NA")
+        return(tibble::data_frame(
+            min_lon = NA_real_,
+            min_lat = NA_real_,
+            max_lon = NA_real_,
+            max_lat = NA_real_
+        ))
+    }
+    if (is.null(features) || length(features) <= 0L) return(default())
+
+    bbox <- sp::bbox(as_sp(geo))
+    if (dim(bbox) != c(2L, 2L) | rownames(bbox) != c("x", "y")) return(default())
+    tibble::data_frame(
+        min_lon = bbox["x", "min"],
+        min_lat = bbox["y", "min"],
+        max_lon = bbox["x", "max"],
+        max_lat = bbox["y", "max"]
+    )
+
 }
 
 #' @export
