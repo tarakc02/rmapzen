39d405d0ba1f1f7cd2bf517e8013c96a1382a1e6
tarakc02 <tarakc02@users.noreply.github.com>
2016-12-21 15:45:23 -0800

    export and document as_sp

--- R/as-sp.R
+++ R/as-sp.R
@@ -1,9 +1,20 @@
+#' Coerce a Mapzen response to an Spatial*DataFrame
+#'
+#' Coerces responses to SpatialPoints (for search responses) or SpatialLines
+#' (for isochrone responses) data frames.
+#'
+#' @param geo The object to be converted
+#' @param ... not currently used
+#'
+#' @export
 as_sp <- function(geo, ...) UseMethod("as_sp")
 
+#' @export
 as_sp.geo_list <- function(geo, ...) {
     geojsonio::geojson_sp(geo, stringsAsFactors = FALSE)
 }
 
+#' @export
 as.data.frame.mapzen_isochrone_list <- function(x, ...) {
 
     coords <- function(feature)
