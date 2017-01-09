70d24ad3d5517b771fc1b45bcd60ef4f533881bd
tarakc02 <tarakc02@github.com>
2017-01-08 20:21:45 -0800

    sample datasets

---
+++ R/data.R
@@ -0,0 +1,25 @@
+#' 25 search results for "Oakland Public library branch"
+#'
+#' Contains the search results from Mapzen's search service for the query
+#' "Oakland public library branch" as of January 8, 2017.
+#'
+#' @format A \code{mapzen_geo_list} with 25 locations
+#'
+#' @source Mapzen, OpenStreetMap, OpenAddresses, GeoNames, WhosOnFirst,
+#' see \url{https://search.mapzen.com/v1/attribution}
+"oakland_public"
+
+#' Pedestrian isochrones from the Berkeley Marina for 10 and 15 minutes
+#'
+#' Isochrone results from Mapzen as of January 8, 2017. The location for the
+#' isochrones is the Berkeley Marina, lat 37.86613, lon -122.3151
+#'
+#' @source Mapzen, OpenStreetMap, British Oceanographic Data Centre,
+#' NASA, USGS, and Transitland.
+"marina_walks"
+
+#' Vector tiles the contain California
+#'
+#' @source Mapzen, OpenStreetMap contributors, Who's On First, Natural Earth,
+#' and openstreetmapdata.com
+"ca_tiles"
