edbc99d2387c54623a2ec9997e72f182d1f4245b
tarakc02 <tarakc02@github.com>
2017-01-10 10:34:01 -0800

    unit tests for as_sp

---
+++ tests/testthat/test-as-sp.R
@@ -0,0 +1,26 @@
+context("as_sp")
+
+test_that("as_sp converts search results to SpatialPointsDF", {
+    oakland_public_sp <- as_sp(oakland_public)
+    expect_is(oakland_public_sp, "SpatialPointsDataFrame")
+})
+
+test_that("as_sp converts isochrone results to SpatialLines/PolygonsDF", {
+    marina_walks_sp <- as_sp(marina_walks)
+    # would by SpatialPolygons if used polygons = TRUE in the mz_isochrone call
+    expect_is(marina_walks_sp, "SpatialLinesDataFrame")
+
+    marina_walks_polygons_sp <- as_sp(marina_walks_polygons)
+    expect_is(marina_walks_polygons_sp, "SpatialPolygonsDataFrame")
+})
+
+test_that("as_sp converts vector tile layers to the appropriate Spatial*DF", {
+    roadlines <- as_sp(ca_tiles$roads, geometry_type = "line")
+    expect_is(roadlines, "SpatialLinesDataFrame")
+
+    earthpolygons <- as_sp(ca_tiles$earth, geometry_type = "polygon")
+    expect_is(earthpolygons, "SpatialPolygonsDataFrame")
+
+    waterpoints <- as_sp(ca_tiles$water, geometry_type = "point")
+    expect_is(waterpoints, "SpatialPointsDataFrame")
+})
