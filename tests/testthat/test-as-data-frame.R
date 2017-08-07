6d956666bb0a4fd51ce224ea0356acb12ab2c8d5
tarakc02 <tarakc02@users.noreply.github.com>
2017-08-07 16:57:06 -0700

    add unit tests for as.data.frame

---
+++ tests/testthat/test-as-data-frame.R
@@ -0,0 +1,17 @@
+context("as.data.frame")
+
+test_that("as.data.frame converts search results to data frame", {
+    oakland_public_df <- as.data.frame(oakland_public)
+    expect_is(oakland_public_df, "data.frame")
+    expect_equal(nrow(oakland_public_df), length(oakland_public$features))
+})
+
+test_that("as.data.frame converts isochrone results to sf", {
+    marina_walks_df <- as.data.frame(marina_walks)
+    expect_is(marina_walks_df, "data.frame")
+    expect_equal(length(unique(marina_walks_df$contour)), length(marina_walks$features))
+    num_features <- vapply(marina_walks$features, function(x) length(x$geometry$coordinates), FUN.VALUE = integer(1))
+    num_features <- sum(num_features)
+    expect_equal(nrow(marina_walks_df), num_features)
+})
+
