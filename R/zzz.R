05536148b61537a89a40595571c4ff8565838578
tarakc02 <tarakc02@github.com>
2018-02-14 08:32:23 -0800

    don't set default providers, error messages should direct user when no provider set

--- R/zzz.R
+++ R/zzz.R
@@ -1,12 +1,12 @@
 .onLoad <- function(libname, pkgname) {
-    rmapzen_default_options <- list(
-        RMAPZEN_MATRIX_HOST = "matrix.mapzen.com",
-        RMAPZEN_SEARCH_HOST = "search.mapzen.com",
-        RMAPZEN_TILE_HOST   = "tile.mapzen.com"
-    )
-    op <- options()
-    toset <- !(names(rmapzen_default_options) %in% names(op))
-    if(any(toset)) options(rmapzen_default_options[toset])
+    # rmapzen_default_options <- list(
+    #     RMAPZEN_MATRIX_HOST = "matrix.mapzen.com",
+    #     RMAPZEN_SEARCH_HOST = "search.mapzen.com",
+    #     RMAPZEN_TILE_HOST   = "tile.mapzen.com"
+    # )
+    # op <- options()
+    # toset <- !(names(rmapzen_default_options) %in% names(op))
+    # if(any(toset)) options(rmapzen_default_options[toset])
 
     invisible()
 }
