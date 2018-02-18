431ebc3875df58e2fc06b3c926ce16d9aa379e79
tarakc02 <tarakc02@github.com>
2018-02-18 14:13:39 -0800

    document and export provider functions

--- R/providers.R
+++ R/providers.R
@@ -1,4 +1,17 @@
-mz_provider <- function(hostname, path = "v1", key = NULL, scheme = "https") {
+#' Configure provider information
+#'
+#' rmapzen works with most implementations of PELIAS. This function defines the
+#' base URL for a particular API provider, and can be used to provider the
+#' \code{provider} argument to \code{\link{mz_set_host}}.
+#'
+#' @param hostname The hostname in the API URL, for instance \code{www.example.com}
+#' @param path Specific path that all API requests must include, e.g. "v1"
+#' @param key API key for this provider, if required
+#' @param scheme The scheme for the URL, should always be "https"
+#'
+#' @seealso \code{\link{mz_set_host}}
+#' @export
+mz_provider <- function(hostname, path = NULL, key = NULL, scheme = "https") {
     if (!assertthat::is.string(hostname)) stop("hostname must be a string")
     if (!is.null(path) && !assertthat::is.string(path))
         stop("path must be a string")
@@ -12,6 +25,19 @@ mz_provider <- function(hostname, path = "v1", key = NULL, scheme = "https") {
          key = key)
 }
 
+#' Set up a host provider for a PELIAS service
+#'
+#' rmapzen works with most implementations of PELIAS. Use this function to set
+#' up the basic information required to connect to a particular provider.
+#' Provider-specific setup functions include information to set up known
+#' providers.
+#'
+#' @param which One of "search", "matrix", or "tile"
+#' @param provider A provider, created using \code{\link{mz_provider}}
+#' @param key API key
+#'
+#' @seealso \code{\link{mz_provider}}
+#' @export
 mz_set_host <- function(which, provider) {
     if (!assertthat::is.string(which)) stop("which must be a string")
     which <- toupper(which)
@@ -22,6 +48,7 @@ mz_set_host <- function(which, provider) {
     options(structure(list(provider), names = optname))
 }
 
+#' @rdname mz_set_host
 mz_get_host <- function(which) {
     if (!assertthat::is.string(which)) stop("which must be a string")
     which <- toupper(which)
@@ -32,13 +59,16 @@ mz_get_host <- function(which) {
     res
 }
 
-mz_set_search_host_geocode.earth <- function(key) {
+#' @rdname mz_set_host
+mz_set_search_host_geocode.earth <- function(key = Sys.getenv("GEOCODE.EARTH_KEY")) {
+    if (is.null(key)) stop("mz_set_search_host_geocode.earth requires a key")
     p <- mz_provider(hostname = "api.geocode.earth",
                      path = "v1",
                      key = key)
     mz_set_host("search", p)
 }
 
+#' @rdname mz_set_host
 mz_set_search_host_nyc_geosearch <- function() {
     p <- mz_provider(hostname = "geosearch.planninglabs.nyc",
                      path = "v1",
@@ -46,7 +76,9 @@ mz_set_search_host_nyc_geosearch <- function() {
     mz_set_host("search", p)
 }
 
-mz_set_tile_host_nextzen <- function(key) {
+#' @rdname mz_set_host
+mz_set_tile_host_nextzen <- function(key = Sys.getenv("NEXTZEN_KEY")) {
+    if (is.null(key)) stop("mz_set_tile_host_nextzen requires a key")
     p <- mz_provider(hostname = "tile.nextzen.org",
                      path = "tilezen/vector/v1",
                      key = key)
