3674c755c2cc4e07cf2216864c8b45c0fb803ae8
tarakc02 <tarakc02@users.noreply.github.com>
2018-04-26 11:42:56 -0700

    when setting up providers, error if key is required and missing

--- R/providers.R
+++ R/providers.R
@@ -62,7 +62,7 @@ mz_get_host <- function(which) {
 #' @rdname mz_set_host
 #' @export
 mz_set_search_host_geocode.earth <- function(key = Sys.getenv("GEOCODE.EARTH_KEY")) {
-    if (is.null(key)) stop("mz_set_search_host_geocode.earth requires a key")
+    if (is.null(key) || key == "") stop("mz_set_search_host_geocode.earth requires a key")
     p <- mz_provider(hostname = "api.geocode.earth",
                      path = "v1",
                      key = key)
@@ -81,7 +81,7 @@ mz_set_search_host_nyc_geosearch <- function() {
 #' @rdname mz_set_host
 #' @export
 mz_set_tile_host_nextzen <- function(key = Sys.getenv("NEXTZEN_KEY")) {
-    if (is.null(key)) stop("mz_set_tile_host_nextzen requires a key")
+    if (is.null(key) || key == "") stop("mz_set_tile_host_nextzen requires a key")
     p <- mz_provider(hostname = "tile.nextzen.org",
                      path = "tilezen/vector/v1",
                      key = key)
